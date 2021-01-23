c5c17e27ba2be663d9834e5ff5560f5d44fb1d4f
Elvis Rodriguez <ElvisRodriguez0621@gmail.com>
2021-01-23 01:18:37 -0500

    fixed link resetting state

--- frontend/src/functions/index.js
+++ frontend/src/functions/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 export const authenticate=(object,type)=>{
-    console.log(type);
     var url="";
     if (type==="signin"){
         url="http://localhost:8080/authenticate";
